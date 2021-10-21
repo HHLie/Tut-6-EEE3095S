683fb38e49e6cd9ce58ff3d5039d8d847251eaa8
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-10-21 14:46:54 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello EEE3095S! \n This is our webserver \n Done by: LXXHSI007 and VBNREE001'
+    return 'Hello EEE3095S! <br/> This is our webserver <br/> Done by: LXXHSI007 and VBNREE001'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
